62d81b2daa9446ba6e1bffe6914aeebff1bb6420
Taylor Ivanoff <taylorivanoff@gmail.com>
2021-09-21 16:53:36 +1000

    Changed title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>IVY WORKS©</title>
+  <title>IVY WORKS</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
